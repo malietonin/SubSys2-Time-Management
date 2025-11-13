d64a7ebdeb12e1b1433f98a6d3c27638d310b6ec
ahmed69k <ahmedassem04@gmail.com>
2025-11-13 18:31:15 +0200

    fixing errors and importing schemas in modules

--- src/shift-assignment/shift-assignment.module.ts
+++ src/shift-assignment/shift-assignment.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ShiftAssignmentSchema } from './models/shift-assignment.model';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ShiftAssignmentController } from './shift-assignment.controller';
-import { ShiftAssignmentService } from './shift-assignment.service';
 
 @Module({
     imports:[MongooseModule.forFeature([{name: 'shift-assignment', schema: ShiftAssignmentSchema}])],
-    controllers: [ShiftAssignmentController],
-    providers: [ShiftAssignmentService]
+    controllers: [],
+    providers: []
 })
 export class ShiftAssignmentModule {}
