e4215230460653f713d00d0b0593ed2f6b018524
malietonin <malak.ayman.saeed@gmail.com>
2025-12-10 19:17:10 +0200

    time exception

---
+++ frontend/app/time-management/attendance/time-exceptions/page.tsx
@@ -0,0 +1,3 @@
+// employees can submit time exceptions 
+// department heads or hr admins can approve/reject requests for their teams
+// department heads or hr admins can escalate unresolved requests to higher management
